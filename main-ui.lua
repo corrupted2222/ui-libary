6dee65ea66157b4889012945f7027fd2e001cd6e
Corrupted <94319249+corrupted2222@users.noreply.github.com>
2024-07-27 22:41:06 -0700

    Update main-ui.lua

--- main-ui.lua
+++ main-ui.lua
@@ -13,6 +13,40 @@ function UILib:CreateWindow(options)
     window.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
     window.BorderSizePixel = 0
 
+
+local UserInputService = game:GetService("UserInputService")
+local frame = window
+local dragging
+local dragInput
+local dragStart
+local startPos
+local function update(input)
+        local delta = input.Position - dragStart
+		frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+end
+frame.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = frame.Position
+                               input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+end)
+frame.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+end)
+
     local titleLabel = Instance.new("TextLabel")
     titleLabel.Parent = window
     titleLabel.Size = UDim2.new(1, 0, 0, 40)
