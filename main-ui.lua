3ecd1defd60d961abeeba9af3be8c471f7837df0
Corrupted <94319249+corrupted2222@users.noreply.github.com>
2024-07-31 17:26:48 -0700

    Update main-ui.lua

--- main-ui.lua
+++ main-ui.lua
@@ -23,6 +23,10 @@ function UILib:CreateWindow(options)
     Header.Position = UDim2.new(0, 0, -0.15, 0)
     Header.Size = UDim2.new(0, 500, 0, 36)
     Header.Active = true
+
+    local HeaderCorners = Instance.new("UICorner")
+    HeaderCorners.CornerRadius = UDim.new(0, 4)
+    HeaderCorners.Parent = Header
     
     local Title = Instance.new("TextLabel")
     Title.Parent = Header
@@ -38,13 +42,44 @@ function UILib:CreateWindow(options)
     
     local GameTitle = Instance.new("TextLabel")
     GameTitle.Parent = Header
-    GameTitle.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-    GameTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
+    GameTitle.BackgroundColor3 = Color3.fromRGB(40,40,40)
+    GameTitle.Size = UDim2.new(0, 200, 0, 30)
+    GameTitle.Position = UDim2.new(0.592, 0, 0.09, 0) 
+    GameTitle.TextColor3 = Color3.fromRGB(255,255,255)
     GameTitle.TextSize = 14
     GameTitle.Text = options.Game or "Game Name"
     GameTitle.Font = Enum.Font.ArialBold
-    GameTitle.BackgroundTransparency = 0
-    
+
+    local GameTitleCorners = Instance.new("UICorner")
+    GameTitleCorners.CornerRadius = UDim.new(0, 4)
+    GameTitleCorners.Parent = GameTitle
+
+local TabsHolder = Instance.new("Frame")
+TabsHolder.Parent = MainFrame
+TabsHolder.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+TabsHolder.Position = UDim2.new(1.025, 0, 0, 0)
+TabsHolder.Size = UDim2.new(0, 54, 0, 300)
+
+local TabsHolderCorners = Instance.new("UICorner")
+TabsHolderCorners.CornerRadius = UDim.new(0, 4)
+TabsHolderCorners.Parent = TabsHolder
+
+local TabsHolderFrame = Instance.new("ScrollingFrame")
+TabsHolderFrame.Parent = TabsHolder
+TabsHolderFrame.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TabsHolderFrame.Position = UDim2.new(0.15, 0, 0.023, 0)
+TabsHolderFrame.Size = UDim2.new(0, 44, 0, 292)
+TabsHolderFrame.CanvasSize = UDim2.new(0, 0, 0, 0) -- change the 3rd one to scroll down --
+TabsHolderFrame.ScrollBarThickness = 0
+TabsHolderFrame.BackgroundTransparency = 1
+TabsHolderFrame.Active = true
+
+local TabsGridLayout = Instance.new("UIGridLayout")
+TabsGridLayout.Parent = TabsHolderFrame
+TabsGridLayout.SortOrder = Enum.SortOrder.LayoutOrder
+TabsGridLayout.CellSize = UDim2.new(0, 37, 0, 37)
+TabsGridLayout.CellPadding = UDim2.new(0, 5, 0, 5) 
+TabsGridLayout.FillDirection = Enum.FillDirection.Vertical
     return ScreenGui
 end
 
