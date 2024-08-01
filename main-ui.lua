0eae96a450e159248695b122ef8291318c1f6f93
Corrupted <94319249+corrupted2222@users.noreply.github.com>
2024-07-31 17:06:14 -0700

    Update main-ui.lua

--- main-ui.lua
+++ main-ui.lua
@@ -1,50 +1,51 @@
 local UILib = {}
 
 function UILib:CreateWindow(options)
-        local ScreenGui = Instance.new("ScreenGui")
-        ScreenGui.Parent = game:GetService("CoreGui")
-        
-        local MainFrame = Instance.new("Frame")
-        MainFrame.Parent = ScreenGui
-        MainFrame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-        MainFrame.Position = UDim2.new(0.5, -100, 0.5, -70)
-        MainFrame.Size = UDim2.new(0, 500, 0, 300)
-        MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-        MainFrame.Active = true
-        MainFrame.Draggable = true
-        
-        local MainFrameCorners = Instance.new("UICorner")
-        MainFrameCorners.CornerRadius = UDim.new(0, 4)
-        MainFrameCorners.Parent = MainFrame
-        
-        local Header = Instance.new("Frame")
-        Header.Parent = MainFrame
-        Header.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-        Header.Position = UDim2.new(0, 0, -0.15, 0)
-        Header.Size = UDim2.new(0, 500, 0, 36)
-        Header.Active = true
-        
-        local Title = Instance.new("TextLabel")
-        Title.Parent = Header
-        Title.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-        Title.Size = UDim2.new(0, 200, 0, 36)
-        Title.Position = UDim2.new(0.01, 0, 0, 0)
-        Title.TextColor3 = Color3.fromRGB(255, 255, 255)
-        Title.TextSize = 17
-        Title.Text = options.Title or "Name Of UI"
-        Title.Font = Enum.Font.ArialBold
-        Title.BackgroundTransparency = 1
-        Title.TextXAlignment = Enum.TextXAlignment.Left
-        
-        local GameTitle = Instance.new("TextLabel")
-        GameTitle.Parent = Header
-        GameTitle.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
-        GameTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
-        GameTitle.TextSize = 14
-        GameTitle.Text = options.Game or "Game Name"
-        GameTitle.Font = Enum.Font.ArialBold
-        GameTitle.BackgroundTransparency = 0
-    return UILib
+    local ScreenGui = Instance.new("ScreenGui")
+    ScreenGui.Parent = game:GetService("CoreGui")
+    
+    local MainFrame = Instance.new("Frame")
+    MainFrame.Parent = ScreenGui
+    MainFrame.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+    MainFrame.Position = UDim2.new(0.5, -100, 0.5, -70)
+    MainFrame.Size = UDim2.new(0, 500, 0, 300)
+    MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
+    MainFrame.Active = true
+    MainFrame.Draggable = true
+    
+    local MainFrameCorners = Instance.new("UICorner")
+    MainFrameCorners.CornerRadius = UDim.new(0, 4)
+    MainFrameCorners.Parent = MainFrame
+    
+    local Header = Instance.new("Frame")
+    Header.Parent = MainFrame
+    Header.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
+    Header.Position = UDim2.new(0, 0, -0.15, 0)
+    Header.Size = UDim2.new(0, 500, 0, 36)
+    Header.Active = true
+    
+    local Title = Instance.new("TextLabel")
+    Title.Parent = Header
+    Title.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+    Title.Size = UDim2.new(0, 200, 0, 36)
+    Title.Position = UDim2.new(0.01, 0, 0, 0)
+    Title.TextColor3 = Color3.fromRGB(255, 255, 255)
+    Title.TextSize = 17
+    Title.Text = options.Title or "Name Of UI"
+    Title.Font = Enum.Font.ArialBold
+    Title.BackgroundTransparency = 1
+    Title.TextXAlignment = Enum.TextXAlignment.Left
+    
+    local GameTitle = Instance.new("TextLabel")
+    GameTitle.Parent = Header
+    GameTitle.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+    GameTitle.TextColor3 = Color3.fromRGB(255, 255, 255)
+    GameTitle.TextSize = 14
+    GameTitle.Text = options.Game or "Game Name"
+    GameTitle.Font = Enum.Font.ArialBold
+    GameTitle.BackgroundTransparency = 0
+    
+    return ScreenGui
 end
 
 return UILib
