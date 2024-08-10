785ff999bc4a381d9c61816dffe5958b25b9abe4
Corrupted <94319249+corrupted2222@users.noreply.github.com>
2024-08-10 15:59:17 -0700

    Update and rename new-ui4.lua to new-ui5.lua

--- new-ui5.lua
+++ new-ui5.lua
@@ -12,7 +12,38 @@ function UILib:CreateWindow(options)
     MainFrame.Size = UDim2.new(0, 500, 0, 300)
     MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
     MainFrame.Active = true
-    MainFrame.Draggable = true
+    local UserInputService = game:GetService("UserInputService")
+local frame = MainFrame
+local dragging
+local dragInput
+local dragStart
+local startPos
+local function update(input)
+        local delta = input.Position - dragStart
+		frame.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+end
+frame.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragging = true
+			dragStart = input.Position
+			startPos = frame.Position
+                               input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragging = false
+				end
+			end)
+		end
+end)
+frame.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			dragInput = input
+		end
+	end)
+UserInputService.InputChanged:Connect(function(input)
+		if input == dragInput and dragging then
+			update(input)
+		end
+end)
 
     local MainFrameCorners = Instance.new("UICorner")
     MainFrameCorners.CornerRadius = UDim.new(0, 4)
