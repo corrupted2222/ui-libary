d36016ae5bf677311b2a8aed80e69d0c970877b2
Corrupted <94319249+corrupted2222@users.noreply.github.com>
2024-08-11 13:28:10 -0700

    Update and rename new-ui1.lua to new-ui2.lua

--- new-ui2.lua
+++ new-ui2.lua
@@ -17,7 +17,7 @@ MainFrame.Draggable = true
 
 -- Check if the device is mobile and adjust the position
 if UserInputService.TouchEnabled then
-    MainFrame.Position = UDim2.new(0.1, 0, 0.5, 0)
+    MainFrame.Position = UDim2.new(0.7, 0, 0.5, 0)
 MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
 end
 	
