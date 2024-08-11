9e0096cc4f5d9e9a5a3046f9428602cfe62a65a6
Corrupted <94319249+corrupted2222@users.noreply.github.com>
2024-08-10 22:27:53 -0700

    Update and rename new-ui7.lua to new-ui8.lua

--- new-ui8.lua
+++ new-ui8.lua
@@ -174,14 +174,13 @@ end)
 if tabOptions.EnableLayout then
     tabContent = Instance.new("ScrollingFrame")
     tabContent.Position = UDim2.new(0.0192, 0, 0.023, 0) -- Ensuring it starts from the top-left
-tabContent.Size = UDim2.new(0, 480, 0, 300) -- Ensuring it fills the parent frame
+tabContent.Size = UDim2.new(0, 680, 0, 500) -- Ensuring it fills the parent frame
     tabContent.ScrollBarThickness = 3
     tabContent.ScrollBarImageColor3 = Color3.fromRGB(255, 255, 255)
     tabContent.ClipsDescendants = true
     tabContent.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
     tabContent.BorderSizePixel = 0
     tabContent.BackgroundTransparency = 0
-    tabContent.CanvasSize = UDim2.new(0, 0, 0, 0)
 
     -- Ensuring it fits within the MainFrame and does not overflow
     tabContent.Parent = MainFrame
