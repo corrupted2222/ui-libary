5ac06825cfb6842291605d7cdf7b95967203ecdc
Corrupted <94319249+corrupted2222@users.noreply.github.com>
2024-08-11 18:39:19 -0700

    Update and rename anti-afk.lua to anti-afk1.lua

--- anti-afk.lua
+++
@@ -1,7 +0,0 @@
-local oldnamecall = nil
-oldnamecall = hookmetamethod(game, "__namecall", newcclosure(function(Self, ...)
-    if (not checkcaller()) and (getnamecallmethod() == "Kick") then
-       return nil
-    end
-    return(oldnamecall(Self, ...))
-end))
